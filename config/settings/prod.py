65283a25edf8bd3112211c8bd5c2da06269af7d0
Dev-Nut00 <sbwoo1106@gmail.com>
2026-01-28 09:59:28 +0900

    fix: CORS 설정 추가 - 프로덕션에서 프론트엔드-백엔드 통신 허용

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -4,6 +4,13 @@ DEBUG = False
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', default=[])
 
+# CORS Settings for production
+CORS_ALLOWED_ORIGINS = [
+    'http://roadmate.p-e.kr',
+    'http://3.34.190.189',
+]
+CORS_ALLOW_CREDENTIALS = True
+
 DATABASES = {
     'default': env.db('DATABASE_URL')
 }
